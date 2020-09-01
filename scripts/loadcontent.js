946de4ca00d057fe2e6887f33c7c091d1ce65f89
Eoin <eoinlynaghgithub@gmail.com>
2020-08-31 22:08:24 -0400

    full jq and nav/footer

--- scripts/loadcontent.js
+++ scripts/loadcontent.js
@@ -1,3 +1,4 @@
 $(function () {
+    $("#navbar").load("https://debatebroshow.github.io/components/headerlocal.html");
     $("#footer").load("https://debatebroshow.github.io/components/footer.html");
 });
