2d1b4407930583a65abd5f13616f885d3d9f4b9f
Eoin <eoinlynaghgithub@gmail.com>
2020-08-31 22:13:47 -0400

    Revert "full jq and nav/footer"
    This reverts commit 946de4ca00d057fe2e6887f33c7c091d1ce65f89.

--- scripts/loadcontent.js
+++ scripts/loadcontent.js
@@ -1,4 +1,3 @@
 $(function () {
-    $("#navbar").load("https://debatebroshow.github.io/components/headerlocal.html");
     $("#footer").load("https://debatebroshow.github.io/components/footer.html");
 });
